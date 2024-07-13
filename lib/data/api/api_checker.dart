1d6a494fe2416c0d8666ff8940cf026da0f91042
Shahidullah191 <shahidullah.cse.gub@gmail.com>
2024-07-13 22:04:56 +0600

    error handle

--- lib/data/api/api_checker.dart
+++ lib/data/api/api_checker.dart
@@ -3,8 +3,44 @@ import 'package:softbd_task/common/custom_snackbar.dart';
 
 class ApiChecker {
   static void checkApi(Response response, {bool showToaster = true}) {
-    if(response.statusCode != 200) {
-      showCustomSnackBar(response.body['message'], showToaster: showToaster);
+    String message;
+    switch (response.statusCode) {
+      case 200:
+        message = "Request was successful.";
+        break;
+      case 201:
+        message = "Resource was created successfully.";
+        break;
+      case 204:
+        message = "No content to display.";
+        break;
+      case 400:
+        message = "Bad request. Please try again.";
+        break;
+      case 401:
+        message = "Unauthorized access. Please log in.";
+        break;
+      case 403:
+        message = "Forbidden. You don't have permission.";
+        break;
+      case 404:
+        message = "Resource not found.";
+        break;
+      case 500:
+        message = "Internal server error. Please try later.";
+        break;
+      case 502:
+        message = "Bad gateway. Please try later.";
+        break;
+      case 503:
+        message = "Service unavailable. Please try later.";
+        break;
+      default:
+        message = "Unexpected error occurred.";
+    }
+
+    if (showToaster) {
+      showCustomSnackBar(message, showToaster: showToaster);
     }
   }
 }
